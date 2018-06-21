f553484d0be12c35c8d1e1f1abab3305aed78a8f
raedsaeed <raedsaeed46@hotmail.com>
2018-06-22 01:47:00 +0200

    Ready for review

---
+++ XYZReader/src/main/java/com/example/xyzreader/FabBehavior.java
@@ -0,0 +1,47 @@
+package com.example.xyzreader;
+
+import android.content.Context;
+import android.support.annotation.NonNull;
+import android.support.design.widget.CoordinatorLayout;
+import android.support.design.widget.FloatingActionButton;
+import android.support.v4.view.ViewCompat;
+import android.util.AttributeSet;
+import android.util.Log;
+import android.view.View;
+
+/**
+ * Created by raed on 3/16/18.
+ */
+
+public class FabBehavior extends FloatingActionButton.Behavior {
+    private static final String TAG = "FabBehavior";
+    public FabBehavior(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    @Override
+    public boolean onStartNestedScroll(@NonNull CoordinatorLayout coordinatorLayout, @NonNull FloatingActionButton child,
+                                       @NonNull View directTargetChild, @NonNull View target, int axes, int type) {
+        return axes == ViewCompat.SCROLL_AXIS_VERTICAL || super.onStartNestedScroll(coordinatorLayout, child, directTargetChild, target, axes, type);
+    }
+
+    @Override
+    public void onNestedScroll(@NonNull CoordinatorLayout coordinatorLayout, @NonNull final FloatingActionButton child,
+                               @NonNull View target, int dxConsumed, int dyConsumed, int dxUnconsumed, int dyUnconsumed, int type) {
+        super.onNestedScroll(coordinatorLayout, child, target, dxConsumed, dyConsumed, dxUnconsumed, dyUnconsumed, type);
+
+        if (dyUnconsumed > 0 && child.getVisibility() == View.VISIBLE) {
+
+            child.hide(new FloatingActionButton.OnVisibilityChangedListener() {
+                @Override
+                public void onHidden(FloatingActionButton fab) {
+                    super.onHidden(fab);
+                    child.setVisibility(View.INVISIBLE);
+                }
+            });
+        } else if (dyUnconsumed <0 && child.getVisibility() != View.VISIBLE) {
+            child.show();
+        }
+    }
+
+}
